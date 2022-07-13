a18cf5d18785891988da94116f9e9639870cff5e
Boyan Todorov <106776456+bopzen@users.noreply.github.com>
2022-07-13 14:44:09 +0300

    Delete 03. Extract File.py

--- 02 Programming Fundamentals with Python May 2022/21 Text Processing - Lab/03. Extract File.py
+++
@@ -1,6 +0,0 @@
-path = input()
-split_path = path.split("\\")
-full_file_name = split_path[-1]
-file_name, file_extension = full_file_name.split(".")
-print(f"File name: {file_name}")
-print(f"File extension: {file_extension}")
