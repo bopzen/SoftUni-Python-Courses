5f31330198b262b3a47c099e293ce4914c08532e
Boyan Todorov <106776456+bopzen@users.noreply.github.com>
2022-07-26 14:31:19 +0300

    Add files via upload

--- 02 Programming Fundamentals with Python May 2022/24 Regular Expressions - Lab/04. Match Numbers.py
+++ 02 Programming Fundamentals with Python May 2022/24 Regular Expressions - Lab/04. Match Numbers.py
@@ -1,7 +1,7 @@
 import re
 
-dates = input()
+numbers = input()
 search_pattern = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
-result = re.finditer(search_pattern, dates)
+result = re.finditer(search_pattern, numbers)
 for match in result:
     print(match.group(), end=" ")
