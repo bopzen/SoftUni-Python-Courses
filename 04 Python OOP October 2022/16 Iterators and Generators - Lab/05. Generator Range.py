6e34409a4a2cf0f3a03f615f1bc922601f879c85
Boyan Todorov <106776456+bopzen@users.noreply.github.com>
2022-11-21 10:07:14 +0200

    Delete 04 Python OOP October 2022/16 Iterators and Generators - Lab directory

--- 04 Python OOP October 2022/16 Iterators and Generators - Lab/05. Generator Range.py
+++
@@ -1,6 +0,0 @@
-def genrange(start, end):
-    i = start
-    n = end
-    while i <= n:
-        yield i
-        i += 1
--- 04 Python OOP October 2022/16 Iterators and Generators - Lab/06. Reverse string.py
+++
@@ -1,5 +0,0 @@
-def reverse_text(text):
-    index = len(text) - 1
-    while index >= 0:
-        yield text[index]
-        index -= 1
