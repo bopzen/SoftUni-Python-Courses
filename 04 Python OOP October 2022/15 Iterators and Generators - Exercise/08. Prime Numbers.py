899ce5c6efb409832e407392d2644247bd6a4b3b
Boyan Todorov <106776456+bopzen@users.noreply.github.com>
2022-11-21 10:07:06 +0200

    Delete 04 Python OOP October 2022/15 Iterators and Generators - Exercise directory

--- 04 Python OOP October 2022/15 Iterators and Generators - Exercise/08. Prime Numbers.py
+++
@@ -1,9 +0,0 @@
-def get_primes(integers):
-    for num in integers:
-        if num <= 1:
-            continue
-        for i in range(2, num):
-            if num % i == 0:
-                break
-        else:
-            yield num
