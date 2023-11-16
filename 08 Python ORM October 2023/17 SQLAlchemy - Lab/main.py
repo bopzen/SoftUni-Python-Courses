7750fa8ce416dfb8bcee7ae48e8c556ba79e1442
Boyan Todorov <106776456+bopzen@users.noreply.github.com>
2023-11-16 09:09:55 +0200

    Add files via upload

---
+++ 08 Python ORM October 2023/17 SQLAlchemy - Lab/main.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = 'postgresql+psycopg2://postgres:Abc123@127.0.0.1/sqlalchemy_db'
+
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
+
+Session = sessionmaker(bind=engine)
